aab62eff96d938c5d2e954df4c82fba6d9648ace
Joan Papajani <joanpapajani47@gmail.com>
2017-06-08 00:15:03 +0200

    db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 20170607221322) do
 
-ActiveRecord::Schema.define(version: 20170608235155) do
 
   create_table "delayed_jobs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.integer "priority", default: 0, null: false
